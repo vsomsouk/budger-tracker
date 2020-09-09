63a9937fb509e3f18bad41c33674e4efabbdb404
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-09-08 21:09:56 -0500

    was having a fetch error with line 1. changed variable to let and issue went away. see if this will deploy correctly

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = [
+let FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
@@ -7,8 +7,8 @@ const FILES_TO_CACHE = [
     "/db.js"
   ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+let CACHE_NAME = "static-cache-v2";
+let DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function(evt) {
